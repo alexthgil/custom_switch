6d573ffde90d2f7ec0b40d6714f670f6d868e349
Alex <alex.nektony@gmail.com>
2021-05-10 20:57:41 +0300

    empty project

---
+++ CustomSwitch/CustomSwitch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomSwitch
+//
+//  Created by Alex on 5/10/21.
+//  Copyright © 2021 AlexCo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
